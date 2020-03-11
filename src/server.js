193630095ebb04c2c6a3ec18f3f9909261378393
FELIPE CHAMONE <chamone8@gmail.com>
2020-03-11 13:02:50 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,8 +8,18 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTION');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    next();
+}
+
 const app = express();
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
+app.use(allowCrossDomain);
 
 
 //inicio da conexão individual 
