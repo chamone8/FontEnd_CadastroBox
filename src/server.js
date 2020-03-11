3d7a7e51d7696cf73d7d6ee8a585eedb9eda573c
FELIPE CHAMONE <chamone8@gmail.com>
2020-03-11 12:57:40 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
 
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 
 
 //inicio da conexão individual 
